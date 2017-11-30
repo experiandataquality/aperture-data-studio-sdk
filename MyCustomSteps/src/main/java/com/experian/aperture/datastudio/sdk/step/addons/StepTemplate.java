d688dd7882ad8818c166e747e240e2ddbac03ef2
Nick Bradley <nick.bradley@experian.com>
2017-11-30 16:25:54 +0000

    Renaming and adding new SDK jars

--- MyCustomSteps/src/main/java/com/experian/aperture/datastudio/sdk/step/addons/StepTemplate.java
+++ MyCustomSteps/src/main/java/com/experian/aperture/datastudio/sdk/step/addons/StepTemplate.java
@@ -13,12 +13,9 @@
  * limitations under the License.
  */
 
-package com.experian.aperture.sdk.step.addons;
+package com.experian.aperture.datastudio.sdk.step.addons;
 
-import com.experian.aperture.sdk.step.StepConfiguration;
-import com.experian.aperture.sdk.step.StepOutput;
-import com.experian.aperture.sdk.step.StepProperty;
-import com.experian.aperture.sdk.step.StepPropertyType;
+import com.experian.aperture.datastudio.sdk.step.*;
 
 import java.util.*;
 
